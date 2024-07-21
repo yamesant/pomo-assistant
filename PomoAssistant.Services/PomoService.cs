5b1ebfb48494a417e5d6535d7ab9cc2b0a6e4662
yamesant <yamesant.try.and.trash@gmail.com>
2024-07-21 17:03:20 +1000

    Implement pomo service

--- PomoAssistant.Services/PomoService.cs
+++ PomoAssistant.Services/PomoService.cs
@@ -1,34 +1,89 @@
+using PomoAssistant.Core;
+
 namespace PomoAssistant.Services;
 
 public sealed class PomoService : IPomoService
 {
+    private readonly List<Pomo> _pomos = [];
+    
     public PomoModels.ReadModel? GetById(Guid id)
     {
-        throw new NotImplementedException();
+        Pomo? pomo = _pomos.FirstOrDefault(p => p.Id == id);
+        return pomo is null ? null : Map(pomo);
     }
 
     public List<PomoModels.ReadModel> Search(PomoModels.SearchRequest request)
     {
-        throw new NotImplementedException();
+        return _pomos
+            .Where(p =>
+                (request.From is null || p.CompletedAt >= request.From.Value) && 
+                (request.To is null || p.CompletedAt <= request.To.Value))
+            .Select(Map)
+            .ToList();
     }
 
     public PomoModels.ReadModel Create(PomoModels.CreateRequest request)
     {
-        throw new NotImplementedException();
+        Pomo pomo = new(request.CompletedAt, request.Intent)
+        {
+            Description = request.Description,
+        };
+        _pomos.Add(pomo);
+        return Map(pomo);
     }
 
     public PomoModels.ReadModel? Update(PomoModels.UpdateRequest request)
     {
-        throw new NotImplementedException();
+        Pomo? pomo = _pomos.FirstOrDefault(p => p.Id == request.Id);
+        if (pomo is null)
+        {
+            return null;
+        }
+        pomo.CompletedAt = request.CompletedAt;
+        pomo.Intent = request.Intent;
+        pomo.Description = request.Description;
+        return Map(pomo);
     }
 
     public PomoModels.ReadModel? Patch(PomoModels.PatchRequest request)
     {
-        throw new NotImplementedException();
+        Pomo? pomo = _pomos.FirstOrDefault(p => p.Id == request.Id);
+        if (pomo is null)
+        {
+            return null;
+        }
+
+        if (request.CompletedAt != null)
+        {
+            pomo.CompletedAt = request.CompletedAt.Value;
+        }
+
+        if (request.Intent != null)
+        {
+            pomo.Intent = request.Intent.Value;
+        }
+
+        if (request.Description != null)
+        {
+            pomo.Description = request.Description;
+        }
+        return Map(pomo);
     }
 
     public void Delete(Guid id)
     {
-        throw new NotImplementedException();
+        _pomos.RemoveAll(p => p.Id == id);
+    }
+
+    private PomoModels.ReadModel Map(Pomo pomo)
+    {
+        return new PomoModels.ReadModel
+        {
+            Id = pomo.Id,
+            CompletedAt = pomo.CompletedAt,
+            Intent = pomo.Intent,
+            Description = pomo.Description,
+            DurationInMinutes = pomo.DurationInMinutes
+        };
     }
 }
